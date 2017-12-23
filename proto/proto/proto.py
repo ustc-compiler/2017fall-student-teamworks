702c646596c716ec14441bcbab57fd7d3a42e68e
nvagus <togemig@hotmail.com>
2017-12-23 18:17:46 +0800

    allow basic instantiate

--- proto/proto/proto.py
+++ proto/proto/proto.py
@@ -4,24 +4,62 @@
 # :time: 12/22/17-3:47 PM
 # :package: proto
 
-from types import MethodType
+
+class Type:
+    def method(self):
+        pass
+
+
+instance = Type()
+method = type(instance.method)
+unbounded = type(Type.method)
 
 
 class Proto(type):
-    def __new__(mcs, init, *args, **kwargs):
-        obj = super(Proto, mcs).__new__(mcs, init.__name__, (mcs,), {})
-        def __new__(cls, *args, **kwargs):
-            obj = super(Proto, cls).__new__()
+    @classmethod
+    def __attr__(mcs, _):
+        yield from ()
+
+    def __new__(mcs, *args, **kwargs):
+        def __attr__(cls, item):
+            result = cls.__proto__.get(item)
+            if result is not None:
+                yield result
+            for __super__ in cls.__super__:
+                yield from __super__.__attr__(item)
+
+        obj = super(Proto, mcs).__new__(
+            mcs, kwargs.pop('__name__'), (mcs,),
+            {
+                '__proto__': {},
+                '__super__': [mcs],
+                '__attr__': __attr__
+            }
+        )
+        obj.__attr__ = method(obj.__attr__, obj)
+        return obj
+
+    def __init__(cls, *args, **kwargs):
+        # pseudo super call
+        super(Proto, cls).__init__(cls, None)
+        # cls.__init_proto__(*args, **kwargs)
 
     def __setattr__(cls, key, value):
         print(key, value)
-        super(Proto, cls).__setattr__(key, value)
+        cls.__proto__[key] = value
+
+    def __getattr__(cls, item):
+        print(item)
+        try:
+            return next(cls.__attr__(item))
+        except StopIteration:
+            pass
+        raise AttributeError(item)
 
 
 def proto(base=Proto):
     def __proto__(f):
-        def _wrapper(*args, **kwargs):
-            obj = base(f, )
-        return _wrapper
+        obj = base(__name__=f.__name__)
+        return obj
 
     return __proto__
--- proto/test/proto.py
+++ proto/test/proto.py
@@ -9,19 +9,19 @@ import code
 from proto import proto
 
 
-@proto
+@proto()
 def person(self, name):
     self._name = name
 
 
-alice = person('Alice')
-
-
-@alice.hook
-def __new__(self, age):
-    self._age = age
-
-
-little_alice = alice(6)
+# alice = person('Alice')
+#
+#
+# @alice.hook
+# def __new__(self, age):
+#     self._age = age
+#
+#
+# little_alice = alice(6)
 
 code.interact(local=locals())
