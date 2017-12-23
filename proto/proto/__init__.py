4ab1e417179ef689a65a0417d2b1819c3f1a0248
Hypercube <hypercube@0x01.me>
2017-12-23 21:35:47 +0800

    remove other files

--- proto/proto/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -- coding: utf8 --
-# :author: nvagus
-# :time: 12/22/17-3:36 PM
-# :package: proto
-
-from .proto import proto
--- proto/test/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -- coding: utf8 --
-# :author: nvagus
-# :time: 12/22/17-5:00 PM
-# :package: proto.test
